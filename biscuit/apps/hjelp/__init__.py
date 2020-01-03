f545d889ce0b726024ae91a1a4c0ca97cbdd1cf5
mirabilos <thorsten.glaser@teckids.org>
2020-01-03 12:30:48 +0100

    Indicate the structure intended after merging

---
+++ biscuit/apps/hjelp/__init__.py
@@ -0,0 +1,3 @@
+# NOTE: this is currently empty, but contains, in subdirectories,
+# two separate Django apps that should be merged into one, those
+# subdirectories faq and support will then be deleted.
